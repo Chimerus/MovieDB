cfeb606a7e15553848864369690a951349b5a4d4
Jason Chan <jlc370@gmail.com>
2016-05-01 19:02:09 -0400

    When user with reviews destroyed, so too are their reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
+  # when movie deleted, its reviews are too
   has_many :reviews, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 require 'gravtastic'
 class User < ApplicationRecord
   include Gravtastic
-
+  # secure password in db with hashing
   has_secure_password
   validates :username, presence: true, uniqueness: {case_sensitive: false}
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, :presence => true, :confirmation =>true
   before_create {generate_token(:auth_token)}
   after_initialize :def
-  has_many :reviews
+  # when user deleted, their reviews are too
+  has_many :reviews, dependent: :destroy
 
   is_gravtastic :default => "mm"
 
